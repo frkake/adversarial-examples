a815b167b6068c8ba33a660db042718b2d000b01
taku-buntu <takumifujinolab@gmail.com>
2019-05-23 17:30:02 +0900

    Initial commit

---
+++ model.py
@@ -0,0 +1,38 @@
+import keras
+from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
+from keras.models import Sequential
+from keras.datasets import mnist
+
+import numpy as np
+from pathlib import Path
+
+def CNN(n_convset=5, n_dense=2, input_shape=(28, 28, 1), num_class=10):
+    model = Sequential()
+
+    model.add(Conv2D(32, (3, 3), input_shape=input_shape, activation="relu", name="conv1"))
+    model.add(Conv2D(32, (3, 3), activation="relu", name="conv2"))
+    model.add(MaxPooling2D(name="mp1"))
+    model.add(Conv2D(32, (3, 3), activation="relu", name="conv3"))
+    model.add(Conv2D(32, (3, 3), activation="relu", name="conv4"))
+    model.add(MaxPooling2D(name="mp2"))
+    model.add(Flatten(name="ft1"))
+    model.add(Dense(256, activation="relu", name="d1"))
+    model.add(Dense(256, activation="relu", name="d2"))
+    model.add(Dense(num_class, activation="softmax", name="d3"))
+
+    return model
+
+if __name__ == "__main__":
+
+    # 学習済みモデルの保存先
+    model_dir = Path("model_dir")
+    model_dir.mkdir(exist_ok=True)
+
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train = np.expand_dims(x_train, axis=-1)
+    x_test = np.expand_dims(x_test, axis=-1)
+
+    model = CNN()
+    model.compile(optimizer="Adam", loss="sparse_categorical_crossentropy", metrics=["acc"])
+    model.fit(x=x_train, y=y_train, epochs=5, validation_data=(x_test, y_test))
+    model.save(str(model_dir/"CNN.h5"))
